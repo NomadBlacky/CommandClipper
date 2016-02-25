9b747a6b39453e5bd0639b6332e1cba52580ab56
nomad blacky <coffee.2milk.2suger@gmail.com>
2016-02-25 22:07:08 +0900

    update test case

--- test/yaml_rspec.rb
+++ test/yaml_rspec.rb
@@ -5,16 +5,36 @@ require 'yaml'
 
 describe YAML do
   let(:yaml) { YAML.load_file(File.dirname(__FILE__) + "/test.yaml") }
+  let(:root) { yaml["root"] }
   
   it "ファイルを読み込む" do
     p yaml
   end
 
-  it "ハッシュ"
+  it "ハッシュ" do
+    hash = root["test_hash"]
+    expect(hash["h1"]).to eq 1
+    expect(hash["h2"]).to eq 2
+    expect(hash["h3"]).to eq ({"h3_1" => 3, "h3_2" => 4})
+  end
 
-  it "配列"
+  it "配列" do
+    ary = root["test_array"]
+    expect(ary.size).to eq 3
+    expect(ary[0]).to eq "a"
+    expect(ary[1]).to eq "b"
+    expect(ary[2]).to eq ["c", "d"]
+  end
 
-  it "boolean"
+  it "boolean" do
+    hash = root["test_boolean"]
+    expect(hash["b1"]).to eq true
+    expect(hash["b2"]).to eq false
+    expect(hash["b3"]).to eq true
+    expect(hash["b4"]).to eq false
+    expect(hash["b5"]).to eq true
+    expect(hash["b6"]).to eq false
+  end
 
   it "整数"
 
