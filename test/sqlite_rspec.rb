9be41ef4898ea97933eec5fc48382e6d122e09b4
blacky <hourou.hamurinzu@gmail.com>
2016-03-06 00:58:28 +0900

    update sqlite3 test case

--- test/sqlite_rspec.rb
+++ test/sqlite_rspec.rb
@@ -23,8 +23,30 @@ describe SQLite3::Database do
   end
 
   it "列名とレコードの取得" do
-    @db.execute2('select * from test where id = 1') do |row|
-      p row
+    head, *rows =  @db.execute2('select * from test where id = 1')
+    expect(head).to eq ["id", "a", "b", "c"]
+    expect(rows).to eq [[1, 1, 2.2, "three"]]
+  end
+
+  it "SELECTした結果の最初のレコードを取得" do
+    result = @db.get_first_row('select * from test order by id')
+    expect(result).to eq [1, 1, 2.2, "three"]
+  end
+
+  it "SELECTした結果の最初のレコードの最初の値を取得する" do
+    result = @db.get_first_value('select count(*) from test where id = 1')
+    expect(result).to eq 1
+  end
+
+  it "トランザクション" do
+    begin
+      @db.transaction do
+        @db.execute('insert into test values(0, 111, 2.22, "san")')
+        raise "hogehoge" #ロールバックされる
+      end #なにもなければここでコミット
+    rescue
+      result = @db.execute('select * from test where id = 0')
+      expect(result).to eq []
     end
   end
   
